e05191df9600fd681c48b841ba273167852d9a17
unknown <Work@GRIFFIN7.econ.local>
2019-03-18 21:34:25 +0100

    Hopen dat dit lukt    -robert

---
+++ workshop/constructors/ingredientObject.js
@@ -0,0 +1,12 @@
+function Ingredient(id, name, amount, type, expirationDate, minimalAmount){
+
+  this.id = id;
+  this.name = name;
+  this.amount = amount;
+  this.type = type;
+  this.expirationDate = expirationDate;
+  this.minimalAmount = minimalAmount;
+
+  return this;
+
+}
---
+++ workshop/constructors/mealObject.js
@@ -0,0 +1,13 @@
+function Meal(id, name, description, season, price, amount){
+  // int, string, string, boolean, double, int, array[ingredienten]
+
+  this.id = id;
+  this.name = name;
+  this.description = description;
+  this.season = season;
+  this.price = price;
+
+  this.ordered = "false";
+  this.time = "12:30";
+
+}
